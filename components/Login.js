04ab8cc6e445c181deb1027726c632b8f7c832ef
sudhakarreddy2000 <32480429+sudhakarreddy2000@users.noreply.github.com>
2023-10-10 13:33:41 +0530

    Add files via upload

--- components/Login.js
+++ components/Login.js
@@ -1,18 +1,18 @@
+import {useState} from 'react'
 function Login() {
-
+const [user, setUser]=useState('')
+const [pass, setPass]=useState('')
     return (
         <>
         <h1>Login</h1>
         <form>
-           <input type="text" placeholder="uid" name="uid"/>
+           <input type="text" placeholder="uid" name="uid" onChange={(e) => setUser(e.target.value)}/>
            <br/>
-           <input type="password" placeholder="password" name="pwd"/>
+           <input type="password" placeholder="password" name="pwd" onChange={(e) => setPass(e.target.value)}/>
            <br/>
-           
            <input type='submit'/>
            </form>
         </>
     );
   }
-  
   export default Login;
--- components/Register.js
+++ components/Register.js
@@ -1,14 +1,17 @@
+import {useState} from 'react'
 function Register() {
-
+    const [user, setUser]=useState('')
+    const [pass, setPass]=useState('')
+    const [rpass, setRpass]=useState('')
     return (
         <>
      <h1>Register</h1>
      <form>
-        <input type="text" placeholder="uid" name="uid"/>
+        <input type="text" placeholder="uid" name="uid" onChange={(e) => setUser(e.target.value)}/>
         <br/>
-        <input type="password" placeholder="password" name="pwd"/>
+        <input type="password" placeholder="password" name="pwd" onChange={(e) => setPass(e.target.value)}/>
         <br/>
-        <input type="password" placeholder="reenter password" name="pwd2"/>
+        <input type="password" placeholder="reenter password" name="pwd2" onChange={(e) => setRpass(e.target.value)}/>
         <br/>
         <input type='submit'/>
         </form>
